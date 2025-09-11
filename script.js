1cde7a558fadafa7e4ce4aa4bc5f1f9c03959e36
morarasospeter <morarasospeter01@gmail.com>
2025-09-11 22:05:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("questionForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    question: document.getElementById("question").value
+  };
+
+  let response = await fetch("http://localhost:5000/api/questions", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+
+  let result = await response.json();
+  document.getElementById("message").innerText = result.message;
+});
